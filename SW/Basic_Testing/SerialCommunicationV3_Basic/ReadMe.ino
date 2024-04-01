5dca7d092b9cc6b180a639af5e28b4bd619d2fe9
ProFilo_is_Back <filippomari18@gmail.com>
2024-04-02 00:36:07 +0200

    Create ReadMe.ino

---
+++ SW/Basic_Testing/SerialCommunicationV3_Basic/ReadMe.ino
@@ -0,0 +1,6 @@
+Versione V£ che non fa alcuna sincronizzazione sui buffer seriali.
+
+nella sezione dei comandi da ESP8266 verso arduino devo un po' migliorare...alcuni comandi li perdo.
+  Forse non dovrei bloccare la lettura appen vedo @...probabilmente dovrei leggere sempre.
+Se si dovessero accodare i comandi va bene...vinceranno gli ultimi che leggo.
+  
