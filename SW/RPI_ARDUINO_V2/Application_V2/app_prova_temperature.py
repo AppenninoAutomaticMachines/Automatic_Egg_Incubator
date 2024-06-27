0ce7f6c2b3bf8847137bbf82eae50aa6c204dcc2
ProFilo_is_Back <filippomari18@gmail.com>
2024-06-27 23:14:44 +0200

    Update app_prova_temperature.py
    aggiunta gestione della memorizzazaione delle temperature sui file
    per fare poi i grafici

--- SW/RPI_ARDUINO_V2/Application_V2/app_prova_temperature.py
+++ SW/RPI_ARDUINO_V2/Application_V2/app_prova_temperature.py
@@ -10,15 +10,18 @@ import psutil  # Import psutil for system statistics
 import easygui
 import os
 import json
-from datetime import datetime
+import csv
+import shutil
+from datetime import datetime, timedelta
+import zipfile
 
-
+import pandas as pd
+import matplotlib.pyplot as plt
 
 # Configure the serial port
-port = "/dev/ttyUSB0"
+port = "COM6"
 baudrate = 19200
 timeout = 0.1
-
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'secret!'
@@ -26,13 +29,16 @@ socketio = SocketIO(app)
 
 temperature_queue = queue.Queue()
 
+
 @app.route('/')
 def index():
     return render_template('index.html')
-    
+
+
 @app.route('/machine_stats')
 def machine_stats():
     return render_template('machine_stats.html')
+
 
 @socketio.on('request_stats')
 def handle_request_stats():
@@ -46,6 +52,7 @@ def handle_request_stats():
         'disk_usage': disk_usage
     }
     emit('machine_stats', stats)
+
 
 def monitoringArduino_task():
     try:
@@ -64,13 +71,14 @@ def monitoringArduino_task():
         except Exception as e:
             print(f"Error closing serial port: {e}")
 
+
 def read_from_arduino(serial_port):
     buffer = ''
     startTransmission = False
     saving = False
     dataDict = {}
     temp_data_list = []
-   
+
     while True:
         try:
             if serial_port.in_waiting > 0:
@@ -86,7 +94,7 @@ def read_from_arduino(serial_port):
                         saving = False
                         decodeMessage(buffer, temp_data_list)
                         buffer = ''
-                        
+
                         if len(temp_data_list) == 3:
                             # se ho collezionato le 3 temperature
                             combined_data = {}
@@ -95,7 +103,7 @@ def read_from_arduino(serial_port):
                             temperature_queue.put(combined_data)
                             temp_data_list.clear()
                     if dataRead == '#':
-                       startTransmission = False
+                        startTransmission = False
         except UnicodeDecodeError as e:
             print(f"Decode error: {e}")
             # Clear the input buffer to avoid repeated errors
@@ -103,20 +111,22 @@ def read_from_arduino(serial_port):
         except serial.SerialException as e:
             print(f"Serial error: {e}")
             break
+
+
 def is_number(s):
     try:
         float(s)  # Try to convert to float
         return True
     except ValueError:
         return False
-        
+
+
 def decodeMessage(buffer, temp_data_list):
     match = re.match(r"<([^,]+),([^>]+)>", buffer)
     if match:
         infoName_part = match.group(1)
         infoData_part = match.group(2)
-        
-                    
+
         if is_number(infoData_part):
             # Convert the number part to float or int
             if '.' in infoData_part:
@@ -127,53 +137,87 @@ def decodeMessage(buffer, temp_data_list):
             # Example actions based on infoName_part
             if infoName_part in ["TMP01", "TMP02", "TMP03"]:
                 temp_data_list.append({infoName_part: number})
-                
+
             # AGGIUNGERE QUI ALTRE INFO CHE VENGONO DA ARDUINO #
         else:
             ciao = False
-            #print("Info Name part:", infoName_part)
-            #print("Info Data part:", infoData_part)  
+            # print("Info Name part:", infoName_part)
+            # print("Info Data part:", infoData_part)
     else:
         print("The input string does not match the expected format.")
+
+def save_temperatures_data(temperatures): #passo un dictionary di temperature, dimensione variabile per gestire più o meno sensori dinamicamente
+    now = datetime.now()
+    current_date = now.strftime('%Y-%m-%d')
+    file_path = os.path.join("Machine_Statistics", f"{current_date}_temperatures.csv")
+
+    # Initialize the CSV file with headers if it doesn't exist
+    if not os.path.exists(file_path):
+        with open(file_path, mode='w', newline='') as file: # write mode
+            writer = csv.writer(file)
+
+            # Initialize the list with 'Timestamp' as the first element
+            result_list = ['Timestamp']
+
+            # Append the keys from the dictionary to the list
+            result_list.extend(temperatures.keys())
+
+            writer.writerow(result_list)
+
+    timestamp = now.strftime('%Y-%m-%d %H:%M:%S')
+
+    with open(file_path, mode='a', newline='') as file: # append mode
+        writer = csv.writer(file)
+
+        # Initialize the list with the timestamp as the first element
+        values_list = [timestamp]
+
+        # Append the values from the dictionary to the list
+        values_list.extend(temperatures.values())
+        writer.writerow(values_list)
 
 
 def periodic_task():
     while True:
         try:
             # Perform other actions here
-            #print("Periodic task is doing other things...")
-            
+            # print("Periodic task is doing other things...")
+
             '''
             Appena arrivano nuove temperature, chiama la funzione che controlla tutta la parte di temperature e heaters.
             + fa memorizzazione dei dati nei file esterni.
             combo box per la modalità di controllo (temperatura massima, media...)
             + fai esclusione delle temperature non lette correttamente, se ci sono.
             in uscita avrai unicamente i comandi di on/off degli attuatori. (le tempistiche le gestisci qui...)
-            
+
             L'invio dei comandi ad arduino lo voglio mantenere asyncrono: stampo i comando dove ne ho bisogno (solo nel periodic task...concorrenza...)
-            
-            
+
+
             chiamata alla funzione ceh controlla lo stepper (guarda se deve girare e se si manda il comando). Controllo parametri
             impostati a video + manuale + lay_horizontal
-            
-            
+
+
             '''
-            
+
             # Check for new data in the queue
             if not temperature_queue.empty():
-                data = temperature_queue.get()
-                
+                currentTemperatures = temperature_queue.get()
+                save_temperatures_data(currentTemperatures) #{'TMP01': 23.1, 'TMP02': 23.1, 'TMP03': 23.1}
+
+
+                data = currentTemperatures
                 # Process the temperature data
                 data.update({'mainHeater': random.choice([True, False])})
                 data.update({'auxHeater': random.choice([True, False])})
                 data.update({'machineState': random.randint(0, 5)})
-                
+
                 data.update({'sendHigherHysteresisLimit_currentValue': random.randint(0, 5)})
                 data.update({'sendLowerHysteresisLimit_currentValue': random.randint(0, 5)})
                 # Send data to HTML page
                 socketio.emit('data_update', data)
         except Exception as e:
             print(f"Error in periodic_task: {e}")
+
 
 def send_async_messages_task_fnct():
     colors = ['red', 'orange', 'yellow', 'green', 'blue']
@@ -195,7 +239,8 @@ def send_async_messages_task_fnct():
         color = random.choice(colors)
         socketio.emit('async_message', {'text': message, 'color': color})
         time.sleep(random.randint(5, 15))
-        
+
+
 def send_async_messages_task_fnct_FILO():
     while True:
         message = "Messaggio da funzione Aync Di Filo"
@@ -217,6 +262,10 @@ def handle_command(data):
         # Handle reset command
         print("Handling reset command")
         socketio.emit('clear_messages')
+
+        plot_all_data('Machine_Statistics')
+
+
     elif data['cmd'] == 'load_parameters':
         print("Handling load_parameters command")
         select_file()
@@ -233,6 +282,7 @@ def handle_number(data):
     # Handle the number as needed
     emit('message', {'text': f'Received number: {number}', 'color': 'blue'})
 
+
 @socketio.on('higherHysteresisLimit')
 def handle_higher_hysteresis_limit(data):
     higher_hysteresis_limit = data.get('higherHysteresisLimit')
@@ -240,43 +290,51 @@ def handle_higher_hysteresis_limit(data):
     # Handle the higher hysteresis limit as needed
     emit('message', {'text': f'Received higher hysteresis limit: {higher_hysteresis_limit}', 'color': 'green'})
 
+
 @socketio.on('lowerHysteresisLimit')
 def handle_lower_hysteresis_limit(data):
     lower_hysteresis_limit = data.get('lowerHysteresisLimit')
     print(f"Received lower hysteresis limit: {lower_hysteresis_limit}")
     # Handle the lower hysteresis limit as needed
     emit('message', {'text': f'Received lower hysteresis limit: {lower_hysteresis_limit}', 'color': 'orange'})
-    
+
+
 @socketio.on('flag1')
 def handle_flag(data):
     flag1 = data.get('flag1', False)
     # Handle the flag logic here
     print(f'Flag1: {flag1}')
-    
+
+
 @socketio.on('flag2')
 def handle_flag(data):
     flag2 = data.get('flag2', False)
     # Handle the flag logic here
     print(f'Flag2: {flag2}')
 
+
 @socketio.on('option')
 def handle_option(data):
     option = data.get('option', '')
     # Handle the option logic here
     print(f'Option selected: {option}')
-   
+
+
 def process_number(number):
     # Implement your logic to process the number
     print(f"Processing number: {number}")
+
 
 def process_higher_hysteresis_limit(limit):
     # Implement your logic to process the higher hysteresis limit
     print(f"Processing higher hysteresis limit: {limit}")
 
+
 def process_lower_hysteresis_limit(limit):
     # Implement your logic to process the lower hysteresis limit
     print(f"Processing lower hysteresis limit: {limit}")
-    
+
+
 def send_async_message(message, color):
     '''
     colors = ['red', 'orange', 'yellow', 'green', 'blue']
@@ -288,11 +346,11 @@ def send_async_message(message, color):
     ]
     '''
     socketio.emit('async_message', {'text': message, 'color': color})
-    
-    
+
+
 def select_file():
     file_path = easygui.fileopenbox()
-    
+
     # Check if a file was selected
     if file_path:
         # Check if the file has a .json extension
@@ -305,15 +363,17 @@ def select_file():
             print("Error: Selected file is not a .json file.")
             send_async_message("Error: Selected file is not a .json file.", 'red')
     else:
-        print("No file selected")   
+        print("No file selected")
         send_async_message("No file selected", 'red')
-        
-#---- HANDLING PERSISTENT PARAMETERS ----#    
+
+
+# ---- HANDLING PERSISTENT PARAMETERS ----#
 def load_json_file(file_path):
     with open(file_path, 'r') as f:
         current_content = json.load(f)
         print(current_content)
         return current_content
+
 
 def get_most_recent_file(folder_path):
     files = [f for f in os.listdir(folder_path) if f.endswith('.json')]
@@ -323,6 +383,7 @@ def get_most_recent_file(folder_path):
     most_recent_file = max(files, key=os.path.getmtime)
     return most_recent_file
 
+
 def load_configuration_startup():
     ''' 
         Questa è la funzione di startup nel senso che è quella che viene chiamata all'avvio.    
@@ -331,7 +392,7 @@ def load_configuration_startup():
     '''
     default_config_folder = 'defaultConfiguration'
     current_config_folder = 'currentConfiguration'
-    
+
     # Check if currentConfiguration folder is empty
     if not os.listdir(current_config_folder):
         print("currentConfiguration folder is empty. Loading from defaultConfiguration.")
@@ -348,12 +409,44 @@ def load_configuration_startup():
         most_recent_file = get_most_recent_file(current_config_folder)
         if not most_recent_file:
             raise FileNotFoundError("No JSON files found in currentConfiguration folder.")
-        return load_json_file(most_recent_file), most_recent_file       
-        
-        
-        
-        
-#--------- GLOBAL VARIABLES SECTION ---------# 
+        return load_json_file(most_recent_file), most_recent_file
+
+
+def plot_all_data(folder_path):
+    all_data = []
+
+    # Read all CSV files in the folder
+    for file_name in os.listdir(folder_path):
+        if file_name.endswith('.csv'):
+            file_path = os.path.join(folder_path, file_name)
+            data = pd.read_csv(file_path, parse_dates=['Timestamp'])
+            all_data.append(data)
+
+    if not all_data:
+        print("No data files found in the folder.")
+        return
+
+    # Concatenate all data
+    concatenated_data = pd.concat(all_data)
+    concatenated_data.sort_values('Timestamp', inplace=True)
+
+    # Plot the data
+    plt.figure(figsize=(10, 6))
+    for column in concatenated_data.columns:
+        if column != 'Timestamp':
+            plt.plot(concatenated_data['Timestamp'], concatenated_data[column], label=column)
+
+    plt.xlabel('Timestamp')
+    plt.ylabel('Values')
+    plt.title('Data from All Files')
+    plt.legend()
+    plt.grid(True)
+    plt.xticks(rotation=45)
+    plt.tight_layout()
+    plt.show(block=False)
+
+
+# --------- GLOBAL VARIABLES SECTION ---------#
 
 
 # PERSISTENT VARIABLES SECTION # 
@@ -383,10 +476,14 @@ print(f"stpr_defaultSpeed: {stpr_defaultSpeed}")
 '''
 print()
 
+# --------- END GLOBAL VARIABLES SECTION ---------#
 
+#--- Create Machine_Statistic folder ---#
+machine_statistics_folder_path = "Machine_Statistics"
 
+if not os.path.exists(machine_statistics_folder_path):
+        os.makedirs(machine_statistics_folder_path)
 
-#--------- END GLOBAL VARIABLES SECTION ---------# 
 
 
 
@@ -395,4 +492,4 @@ if __name__ == '__main__':
     threading.Thread(target=periodic_task).start()
     threading.Thread(target=send_async_messages_task_fnct).start()
     threading.Thread(target=send_async_messages_task_fnct_FILO).start()
-    socketio.run(app, host='0.0.0.0', port=5000)
+    socketio.run(app, host='0.0.0.0', port=5000) #, allow_unsafe_werkzeug=True) - per nuove versioni di flask socket IO
